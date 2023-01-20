dc49d09644b762258137d1207967d0f675f81d65
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-20 17:41:23 +0530

    Add files via upload

---
+++ MVC2-MiniProjectApp/src/main/java/com/nt/service/IEmployeeMgmtService.java
@@ -0,0 +1,10 @@
+package com.nt.service;
+
+import java.util.List;
+
+import com.nt.model.Employee;
+
+
+public interface IEmployeeMgmtService {
+	public  List<Employee> fetchEmpsByDesg(String desg)throws Exception;
+}
