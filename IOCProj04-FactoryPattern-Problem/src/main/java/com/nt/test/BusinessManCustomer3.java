208ec7dc2fe2648d84adb3cc7406a84294fc48ba
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-20 17:28:24 +0530

    Add files via upload

---
+++ IOCProj04-FactoryPattern-Problem/src/main/java/com/nt/test/BusinessManCustomer3.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+import com.nt.comp.Car;
+import com.nt.comp.LuxoryCar;
+
+public class BusinessManCustomer3 {
+
+	public static void main(String[] args) {
+		Car car=new LuxoryCar("TS11 AB 5151");
+		car.drive();
+	}
+}
---
+++ IOCProj04-FactoryPattern-Problem/src/main/java/com/nt/test/YouthCustomer2.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+import com.nt.comp.Car;
+import com.nt.comp.SportsCar;
+
+public class YouthCustomer2 {
+
+	public static void main(String[] args) {
+		Car car=new SportsCar("TS10 KK 5656");
+		car.drive();
+	}
+}
