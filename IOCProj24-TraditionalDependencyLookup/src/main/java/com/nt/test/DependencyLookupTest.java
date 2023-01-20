508a8b872e689a5aa9df4ccfbfd502f605c0b668
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-20 17:38:29 +0530

    Add files via upload

---
+++ IOCProj24-TraditionalDependencyLookup/src/main/java/com/nt/test/DependencyLookupTest.java
@@ -0,0 +1,18 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.beans.Cricketer;
+
+public class DependencyLookupTest {
+	public static void main(String[] args) {
+  //create IOC container
+	ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+	//get target class
+	Cricketer player=ctx.getBean("cktr",Cricketer.class);
+	//invoke methods
+	player.batting();
+	player.bowling();
+	player.fielding();
+	}
+}
